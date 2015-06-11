44e5af1a5ece6291cdb04bd65ec0e1dceab14201
Nathan Gouy <nathangouy@free.fr>
2015-06-11 18:20:41 +0200

    all clean

--- newclasscpp/makeRead.sh
+++ newclasscpp/makeRead.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-rm -rf README
+rm -rf README.txt
 
-echo -e "create two files such as if i want to create a new class called \"foo\" :\n\n$ cncpp foo\n" >> README
-./cncpp.sh "foo" >> README
-echo -e "\n\$cat Foo.class.hpp" >> README
-cat Foo.class.hpp >> README
-echo -e "\n\$cat Foo.class.cpp" >> README
-cat Foo.class.cpp >> README
+echo -e "create two files such as if i want to create a new class called \"foo\" :\n\n$ cncpp foo\n" >> README.txt
+./cncpp.sh "foo" >> README.txt
+echo -e "\n\$cat Foo.class.hpp" >> README.txt
+cat Foo.class.hpp >> README.txt
+echo -e "\n\$cat Foo.class.cpp" >> README.txt
+cat Foo.class.cpp >> README.txt
 
 rm -rf Foo.class.hpp
 rm -rf Foo.class.cpp
